14d3dd0c660ac90d0368e889319701779b6f8391
haoningng <nick.nghn@gmail.com>
2024-04-15 19:19:33 +1000

    feat: add task3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module gather data form an API"""
+import requests
+import json
+
+
+all_todos = requests.get(
+    'https://jsonplaceholder.typicode.com/todos')
+
+done_user = []
+user_todo = {}
+
+for each in all_todos.json():
+    USER_ID = each["userId"]
+    while (USER_ID not in done_user):
+        done_user.append(USER_ID)
+        user = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(USER_ID))
+        USERNAME = user.json()["username"]
+        todos = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/todos'
+            .format(USER_ID))
+        data_list = []
+        for each in todos.json():
+            data = {}  # Create a new dictionary for each iteration
+            data["username"] = str(USERNAME)
+            data["task"] = str(each["title"])
+            data["completed"] = each["completed"]
+            data_list.append(data)
+        user_todo[str(USER_ID)] = data_list
+
+json_file = 'todo_all_employees.json'
+
+with open(json_file, 'w') as file:
+    json.dump(user_todo, file)
