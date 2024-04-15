ade824ed0b2bbab696e97381dc1b5e4a77cc1759
haoningng <nick.nghn@gmail.com>
2024-04-15 18:50:17 +1000

    feat: add task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module gather data form an API"""
+import requests
+import csv
+import sys
+
+USER_ID = sys.argv[1]
+user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                    .format(USER_ID))
+todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(USER_ID))
+
+USERNAME = user.json()["username"]
+
+data_list = []
+
+for each in todos.json():
+    data = {}  # Create a new dictionary for each iteration
+    data["USER_ID"] = str(USER_ID)
+    data["USERNAME"] = str(USERNAME)
+    data["TASK_COMPLETED_STATUS"] = str(each["completed"])
+    data["TASK_TITLE"] = str(each["title"])
+    data_list.append(data)
+
+keys = data.keys()
+csv_file = '{}.csv'.format(USER_ID)
+
+with open(csv_file, 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=keys, quoting=csv.QUOTE_ALL)
+    for row in data_list:
+        writer.writerow(row)
